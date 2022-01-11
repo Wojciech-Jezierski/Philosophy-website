6da494a7cccb5e5d336319da6e311a154c18ebcb
Primo20 <wojtek20000@poczta.onet.pl>
2022-01-11 22:53:14 +0100

    update

---
+++ app.js
@@ -0,0 +1,13 @@
+const btnScrollToTop = document.querySelector(".scrollTop");
+
+        btnScrollToTop.addEventListener("click", function () {
+            // window.scrollTo(0, 0);
+
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: "smooth"
+            });
+
+            // $("html, body").animate({ scrollTop: 0 }, "slow");
+        });
